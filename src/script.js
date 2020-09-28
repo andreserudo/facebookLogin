acac9921b84457abcd61e346aa69485c832091be
Andre Serudo <andreserudo@gmail.com>
2020-09-27 21:20:48 -0400

    adding years in the select item

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@
 function loadBirthDate(){
   const selectDayBirth = document.getElementById('dayBirth');
   const selectMonthBirth = document.getElementById('monthBirth');
+  const selectYearBirth = document.getElementById('yearBirth');
   const months = [
     'Janeiro',
    'Fevereiro', 
@@ -15,6 +16,11 @@ function loadBirthDate(){
    'Outubro', 
    'Novembro', 
    'Dezembro'];
+  const today = new Date();
+  const day = today.getDate();
+  const month = today.getMonth();
+  const year = today.getFullYear();
+  const initialYear = year - 100;
 
   for(let i=0; i<31; i+= 1){
     let newDay = document.createElement('option');
@@ -27,8 +33,17 @@ function loadBirthDate(){
     let newMonth = document.createElement('option');    
 
     newMonth.innerText = months[i];
+
     selectMonthBirth.append(newMonth);
   }  
+
+  for(let i=initialYear; i<=year; i+= 1){
+    let newYear = document.createElement('option');
+
+    newYear.innerText = String(i);
+    selectYearBirth.append(newYear);
+  }
+  
 }
 
 function createEvents(){
